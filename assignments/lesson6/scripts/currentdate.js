8f50718e59b0bf2e48e98d0b19d24b06a4deef20
Cassie White <cassieannwhite@gmail.com>
2018-06-02 14:30:59 -0700

    date

--- assignments/lesson6/scripts/currentdate.js
+++ assignments/lesson6/scripts/currentdate.js
@@ -1,17 +1,16 @@
-.mydate {
-var today = new Date();
-var dd = today.getDate();
-var mm = today.getMonth()+1; //January is 0!
-var yyyy = today.getFullYear();
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
 
-if(dd<10) {
-    dd = '0'+dd
-} 
+    var n = weekday[d.getDay()];
+    var d = getDate();
+    var m = getMonth();
+    var y = getFullYear();
 
-if(mm<10) {
-    mm = '0'+mm
-} 
 
-today = mm + '/' + dd + '/' + yyyy;
-document.write("mydate");
-}
+   
