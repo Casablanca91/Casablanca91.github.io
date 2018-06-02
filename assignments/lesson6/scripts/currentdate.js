7a26c06fa42e5228863f3c188b86f185a67bbb49
Cassie White <cassieannwhite@gmail.com>
2018-06-02 16:04:50 -0700

    k

--- assignments/lesson6/scripts/currentdate.js
+++ assignments/lesson6/scripts/currentdate.js
@@ -1,17 +1,18 @@
 {
-    var d = new Date();
-    var weekday = new Array(7);
-    weekday[0] = "Sunday";
-    weekday[1] = "Monday";
-    weekday[2] = "Tuesday";
-    weekday[3] = "Wednesday";
-    weekday[4] = "Thursday";
-    weekday[5] = "Friday";
-    weekday[6] = "Saturday";
+var d = new Date();
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
 
-    var n = weekday[d.getDay()];
-    var x = x.getDate();
-    var m = m.getMonth();
-    var y = y.getFullYear();
-    document.getElementById("currentdate").innerHTML = n + "," + x + m + y;
+var n = weekday[d.getDay()];
+var x = x.getDate();
+var m = m.getMonth();
+var y = y.getFullYear();
 }
+
+document.getElementById("currentdate").innerHTML = n + "," + x + m + y;
