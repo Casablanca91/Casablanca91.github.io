691eb254c4a159f66369fabd3ec398f8642d9e58
Cassie White <cassieannwhite@gmail.com>
2018-07-01 19:57:27 -0700

    weatherdata

---
+++ assignments/weathersite/scripts/weatherspring.js
@@ -0,0 +1,17 @@
+//connection to the api
+var weatherRequest = new XMLHttpRequest();
+
+var apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5388881&units=imperial&APPID=083564803af7abcd205ad149b46ed4ab";
+
+weatherRequest.open("Get", apiURL, true);
+weatherRequest.send();
+
+weatherRequest.onload = function () {
+    var weatherData = JSON.parse(weatherRequest.responseText);
+    //console.log(weatherData);
+    document.getElementById("current-temp").innerHTML = weatherData.main.temp;
+    document.getElementById("wdescription").innerHTML = weatherData.main.description;
+    document.getElementById("windspeed").innerHTML = weatherData.wind.speed;
+    var imagesrc = "https://openweathermap.org/img/w/" + weatherData.weather(0).icon + "png";
+    document.getElementById("weatherimage").src = imagesrc;
+}
