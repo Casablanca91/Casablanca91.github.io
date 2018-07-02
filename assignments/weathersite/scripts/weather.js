94fbbe44b3856e3be934565b2035d468daaa4938
Cassie White <cassieannwhite@gmail.com>
2018-07-01 19:38:29 -0700

    update

--- assignments/weathersite/scripts/weather.js
+++ assignments/weathersite/scripts/weather.js
@@ -1,7 +1,7 @@
 //connection to the api
 var weatherRequest = new XMLHttpRequest();
 
-var apiURL = "http://api.openweathermap.org/data/2.5/weather?id=4156210&units=imperial&APPID=083564803af7abcd205ad149b46ed4ab";
+var apiURL = "https://api.openweathermap.org/data/2.5/weather?id=4156210&units=imperial&APPID=083564803af7abcd205ad149b46ed4ab";
 
 weatherRequest.open("Get", apiURL, true);
 weatherRequest.send();
@@ -10,8 +10,8 @@ weatherRequest.onload = function () {
     var weatherData = JSON.parse(weatherRequest.responseText);
     //console.log(weatherData);
     document.getElementById("current-temp").innerHTML = weatherData.main.temp;
-    document.getElementById("wdescription").innerHTML = weatherData.weather.description;
+    document.getElementById("wdescription").innerHTML = weatherData.main.description;
     document.getElementById("windspeed").innerHTML = weatherData.wind.speed;
-    var imagesrc = "http//openweathermap.org/img/w/" + weatherdata.weather(0).icon + "png";
+    var imagesrc = "https://openweathermap.org/img/w/" + weatherData.weather(0).icon + "png";
     document.getElementById("weatherimage").src = imagesrc;
 }
